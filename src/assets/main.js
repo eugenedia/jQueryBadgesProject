7e1a63275838fcfd36ebb9775c991c8bf2395533
Eugene Dia <EugeneDia@gmail.com>
2017-03-30 06:14:31 +0300

    First commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url:'https://www.codeschool.com/users/1550944.json',
+    datatype: 'jsonp',
+    success: function(response){
+      for (var i=0; i<response.courses.completed.length; i++){
+        var course =  response.courses.completed[i];
+        var courseDiv = $( "<div><h3>" +course.title+ "</h3><div>"+
+          "<img  src=\"" + course.badge + "\">");
+
+        var link = $("<a href=\""+ course.url +"\" target = \"blank\" >See course</a>").addClass("btn btn-primary");
+
+         courseDiv.append(link);
+         courseDiv.addClass('cource').appendTo("#badges");
+      }
+    }
+  })
 
 });
